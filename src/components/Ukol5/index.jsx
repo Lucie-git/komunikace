a01ae32937dff99ca10dc0290a19d985e97b6a89
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-12 14:17:10 +0200

    last task solved

--- src/components/Ukol5/index.jsx
+++ src/components/Ukol5/index.jsx
@@ -1,17 +1,35 @@
-import { InputField } from "./InputField";
+import { useState } from 'react';
+import { InputField } from './InputField';
 
 /*
-Zadání: Vyřešte tento úkol podobně jako předchozí, pouze s tím rozdílem, že tentokrát budete mít
+ Zadání: Vyřešte tento úkol podobně jako předchozí, pouze s tím rozdílem, že tentokrát budete mít
   dvě pole, do kterých uživatel může psát jméno a příjmení. Výsledné jméno a příjmení se mají
   zobrazit pod nadpisem oddělené mezerou. Budete tedy potřebovat dva stavy, každý pro jedno pole.
 */
 
 export const Ukol5 = () => {
+  const [firstName, setFirstName] = useState('');
+  const [secondName, setSecondName] = useState('');
+
+  const handleFirstName = (inputValue) => {
+    setFirstName(inputValue);
+  };
+  const handleSecondName = (inputValue) => {
+    setSecondName(inputValue);
+  };
   return (
     <>
-      <p>Jméno: Patrik Pokoutník</p>
-      <InputField label="Jméno" type="text" />
-      <InputField label="Příjmení" type="text" />
+      <p>
+        Jméno: {firstName} {secondName}
+      </p>
+      <InputField label="Jméno" type="text" onValueChange={handleFirstName} value={firstName} />
+
+      <InputField
+        label="Příjmení"
+        type="text"
+        onValueChange={handleSecondName}
+        value={secondName}
+      />
     </>
   );
 };
